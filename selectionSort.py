3f3b51439f5a2febbdcf4d5b6217c23105f7758e
Madhav Chekka <madhav.chekka@gmail.com>
2021-07-11 18:53:20 -0700

    initiate index to None

--- selectionSort.py
+++ selectionSort.py
@@ -4,23 +4,25 @@ def selectionSort(a):
     # end of the loop, swap the minimum with first unsorted position
 
     for i in range(0,len(a)-1):
+
         minElem = a[i]
         j=i+1
+        index = None
 
         while j < len(a):
             if a[j] < minElem:
                 minElem = a[j]
                 index = j
             j+=1
-
-        a[i],a[index] = minElem,a[i]
+        if index:
+            a[i],a[index] = minElem,a[i]
 
     return a
 
 
 
 if __name__ == '__main__':
-    arrA = [3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
+    arrA = [1,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
     print(f'Input array: {arrA}')
     # selectionSort(arrA)
     sortedA = selectionSort(arrA)
